748bb77ca706543df3cdea3ddfed08944eb07bd6
雨希 张 <2512061959@qq.com>
2024-07-23 15:10:18 +0800

    项目初始化

---
+++ src/main/java/org/example/usercenter/demos/UserCenterApplication.java
@@ -0,0 +1,14 @@
+package org.example.usercenter.demos;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.example.usercenter.demos.mapper")
+
+public class UserCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+}
